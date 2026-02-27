2aa0d54e5361997246d375e1306e3d69cd9a892c
Amlal El Mahrouss <amlal@nekernel.org>
2026-02-27 06:53:15 +0100

    chore: update print.hpp, add asio.hpp helper header.
    Signed-off-by: Amlal El Mahrouss <amlal@nekernel.org>

---
+++ include/ocl/asio.hpp
@@ -0,0 +1,15 @@
+// Copyright 2026, Amlal El Mahrouss (amlal@nekernel.org)
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+// Official repository: https://github.com/ocl-org/core
+
+#ifndef OCL_CORE_ASIO
+#define OCL_CORE_ASIO
+
+#include <boost/asio/co_spawn.hpp>
+#include <boost/asio/detached.hpp>
+#include <boost/asio/io_context.hpp>
+#include <boost/asio/write.hpp>
+#include <boost/asio/read.hpp>
+
+#endif
