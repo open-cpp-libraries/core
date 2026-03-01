64d48970ba5a8edb9414a5f8ee2ed6daa022879a
Amlal El Mahrouss <amlal@nekernel.org>
2026-03-01 19:53:56 +0100

    chore: rename allocator_op example.
    Signed-off-by: Amlal El Mahrouss <amlal@nekernel.org>

--- example/allocator_op_example/example.cpp
+++ example/allocator_op_example/example.cpp
@@ -1,4 +1,4 @@
-// Copyright 2025, Amlal El Mahrouss (amlal@nekernel.org)
+// Copyright 2025-2026, Amlal El Mahrouss (amlal@nekernel.org)
 // Distributed under the Boost Software License, Version 1.0. (See accompanying
 // file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 // Official repository: https://github.com/ocl-org/core
