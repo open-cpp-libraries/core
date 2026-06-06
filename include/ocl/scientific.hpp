9f440b9e3701ca0851efbeb3aebf3c15c7460444
Amlal El Mahrouss <amlal@nekernel.org>
2026-06-06 17:56:35 +0200

    [CHORE] src: update scientific module.
    Signed-off-by: Amlal El Mahrouss <amlal@nekernel.org>

--- include/ocl/scientific.hpp
+++ include/ocl/scientific.hpp
@@ -8,17 +8,44 @@
 
 #include <ocl/detail/math/config.hpp>
 
-#ifndef zeta_apery
-#define zeta_apery zeta_three
-#endif
-
 namespace ocl::scientific 
 {
     
-    using quaternionf = boost::math::quaternion<float>;
-	using quaterniond = boost::math::quaternion<double>;
+    using quaternionf = ::boost::math::quaternion<float>;
+	using quaterniond = ::boost::math::quaternion<double>;
 
-    inline auto zeta_apery_f = boost::math::zeta<float>(3.0f);
-	inline auto zeta_apery_d = boost::math::zeta<double>(3.0);
+    /// @brief The value of the Riemann zeta function at 3, also known as Apery's constant.
+    inline auto zeta_apery_f = ::boost::math::zeta<float>(3.0f);
+	inline auto zeta_apery_d = ::boost::math::zeta<double>(3.0);
+
+    /// @brief Enumeration to specify the type of space for quaternion norm computation.
+    enum class space_type : int32_t
+	{
+		euclidean,
+		minkowski
+	};
+
+    /// @brief Computes the norm of a quaternion in the specified space.
+	/// @tparam T The type of the quaternion components (e.g., float, double).
+	/// @tparam S The type of space (euclidean or minkowski) to compute the norm in.
+	/// @param q The quaternion for which to compute the norm.
+	/// @return The norm of the quaternion in the specified space.
+	template <typename T, space_type S>
+	inline T norm(quaternion<T> const& q)
+	{
+		switch (S)
+		{
+		case space_type::euclidean:
+			return ::boost::math::abs(q);
+		case space_type::minkowski:
+			return ::boost::math::norm(q);
+		}
+
+        throw std::invalid_argument("Invalid space type");
+    }
 
 } // namespace ocl::scientific
+
+#ifndef zeta_apery
+#define zeta_apery zeta_three
+#endif
