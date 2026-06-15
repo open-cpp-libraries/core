bbc83bf9958d22e7835cf0db3c01d3e3729b03c8
Amlal El Mahrouss <amlal@nekernel.org>
2026-06-15 21:01:49 +0200

    [CHORE] src: great improvements to library.

--- include/ocl/scientific.hpp
+++ include/ocl/scientific.hpp
@@ -2,7 +2,7 @@
 // Copyright 2026, Amlal El Mahrouss (amlal@nekernel.org)
 // Distributed under the Boost Software License, Version 1.0. (See accompanying
 // file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
-// Official repository: https://github.com/ocl-foss-org/core
+// Official repository: https://github.com/open-cpp-libraries/core
 
 #pragma once
 
@@ -41,7 +41,7 @@ namespace ocl::scientific
 			return ::boost::math::norm(q);
 		}
 
-        throw std::invalid_argument("Invalid space type");
+        ::boost::throw_exception(std::runtime_error("Invalid type"), BOOST_CURRENT_LOCATION);
     }
 
 } // namespace ocl::scientific
--- include/ocl/smart_ptr.hpp
+++ include/ocl/smart_ptr.hpp
@@ -2,7 +2,7 @@
 // Copyright 2025-2026, Amlal El Mahrouss (amlal@nekernel.org)
 // Distributed under the Boost Software License, Version 1.0. (See accompanying
 // file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
-// Official repository: https://github.com/ocl-foss-org/core
+// Official repository: https://github.com/open-cpp-libraries/core
 
 #ifndef OCL_SMART_PTR
 #define OCL_SMART_PTR
