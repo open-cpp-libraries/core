ee422181c1560a299ad3abe925daedc98e50650c
Amlal El Mahrouss <amlal@nekernel.org>
2026-07-02 09:19:05 +0200

    [CHORE] src: examples proof-reading.
    Signed-off-by: Amlal El Mahrouss <amlal@nekernel.org>

--- example/math_example/example.ex.cpp
+++ example/math_example/example.ex.cpp
@@ -15,7 +15,7 @@ auto main(int argc, char** argv) -> int
 	ocl::scientific::eq_solver_two solver;
 	std::cout << solver(1, 2 + 2) << std::endl; // false
 	std::cout << solver(4, 2 + 2) << std::endl;					// true
-	std::cout << solver(1+1, 2) << std::endl;					// true
+	std::cout << solver(1 + 1, 2) << std::endl;					// true
 	std::cout << solver(sqrt_two, (2 / sqrt_two)) << std::endl; // false, normally it is true, but the solver solves (2 / 2^(1/12)) first and then compares the result to 2^(1/12).
 
 	return EXIT_SUCCESS;
