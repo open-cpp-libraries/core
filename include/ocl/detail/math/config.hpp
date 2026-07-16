b35f8cbb41031a2de6b773183b7b2aab46423301
Amlal El Mahrouss <amlal@nekernel.org>
2026-07-16 22:26:20 +0200

    [FEAT] src: updated config.hpp

--- include/ocl/detail/math/config.hpp
+++ include/ocl/detail/math/config.hpp
@@ -9,6 +9,7 @@
 #include <boost/math/quaternion.hpp>
 #include <boost/math/special_functions.hpp>
 #include <boost/math/constants/constants.hpp>
+#include <cmath>
 
 #define OCL_MATH_MODULE (202606)
 
@@ -25,7 +26,6 @@ namespace ocl::scientific
 
 	} // namespace detail
 
-	
 	using real_type = double;
 
 	inline const real_type add(const real_type& left, const real_type& right)
@@ -51,6 +51,11 @@ namespace ocl::scientific
 	inline const real_type pow(const real_type& left, const real_type& right)
 	{
 		return std::pow(left, right);
+	}
+
+	inline const real_type sqrt(const real_type& left, const real_type& right = 0)
+	{
+		return std::sqrt(left);
 	}
 
 } // namespace ocl
