bfb42de93f3b9a38b777014e421d2a4696d4f79c
Amlal El Mahrouss <amlal@nekernel.org>
2026-07-24 16:23:50 +0200

    [CHORE] src: tweak math.test.cpp module.
    Signed-off-by: Amlal El Mahrouss <amlal@nekernel.org>

--- test/std/math.test.cpp
+++ test/std/math.test.cpp
@@ -16,8 +16,10 @@ BOOST_AUTO_TEST_CASE(math_should_solve_eq)
 	ocl::scientific::eq_solver_two solver;
 
     bool r1 = solver(1, {ocl::scientific::add(2, 2)}) == 0;
+
 	BOOST_TEST(r1 == false);
     r1 = solver(4, {ocl::scientific::add(2, 2)}) != 0;
+
 	BOOST_TEST(r1 == false);
     r1 = solver(ocl::scientific::add(1, 1), {2}) == 0;
 	BOOST_TEST(r1 == true);
