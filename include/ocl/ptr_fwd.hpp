43a991da71ebd0d4205de66352ad898335531b75
Amlal El Mahrouss <amlal@nekernel.org>
2026-09-15 08:52:40 +0200

    FEAT: src: include ptr_fwd declarations file

---
+++ include/ocl/ptr_fwd.hpp
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: BSL-1.0
+// Copyright 2025-2026, Amlal El Mahrouss (amlal@nekernel.org)
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+// Official repository: https://github.com/open-cpp-libraries/core
+
+#ifndef OCL_SMART_PTR_FWD
+#define OCL_SMART_PTR_FWD
+
+#include <ocl/detail/config.hpp>
+#include <ocl/smart_ptr.hpp>
+#include <ocl/tracked_ptr.hpp>
+
+#endif
